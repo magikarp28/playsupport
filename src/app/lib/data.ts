80f9ef3df7458e8d8a314883fd8d21882e895fe0
Magikarp28 <filipknezovi@gmail.com>
2024-11-13 16:03:02 +0100

    main

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,7 +1,3 @@
-import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
 import supportedSoldier from "@/../public/SupportedSoldier.svg";
 
 export const PaintingShowcaseData = [
