e1ea712a93cf9f2aff5f7af2415c5b753ea2de58
Filip Knezović <filipknezovi@gmail.com>
2025-02-01 22:21:26 +0100

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import ActiveSectionContextProvider from "@/components/context/active-section-context";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,6 +40,8 @@ export default function RootLayout({
             {children}
           </ActiveSectionContextProvider>
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
