42c791c8a4c59044e22a4746200c951e40217093
Filip Knezović <filipknezovi@gmail.com>
2025-03-07 17:31:07 +0100

    pls gmail proradi

--- src/lib/ServerActions.ts
+++ src/lib/ServerActions.ts
@@ -9,8 +9,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   host: process.env.STMP_SERVER,
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
