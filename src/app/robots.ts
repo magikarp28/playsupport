8a36e1a8cd0384cc6c97bcdeead6ac453d837b73
Magikarp28 <filipknezovi@gmail.com>
2026-01-27 16:18:44 +0100

    seo more

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/privacypolicy"],
+    },
+    sitemap: "https://playsupport.art/sitemap.xml",
+    host: "https://playsupport.art/sitemap.xml",
+  };
+}
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,38 @@
+import { url } from "inspector";
 import { MetadataRoute } from "next";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://playsupport.art";
   return [
     {
-      url: "https://playsupport.art",
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/painting`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/gallery`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/supporting`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.2,
     },
   ];
 }
